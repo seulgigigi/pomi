4e77fc15b1310abce4f499f839ebe3c77227fa76
seulgigigi <157334032+seulgigigi@users.noreply.github.com>
2024-01-23 12:23:15 +0800

    Update service.js

--- service.js
+++ service.js
@@ -9,17 +9,20 @@ const filesToCache = [
 ];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(cacheName).then((cache) => {
-      return cache.addAll(filesToCache);
-    })
-  );
+    console.log('Service Worker installed');
+    e.waitUntil(
+        caches.open(cacheName).then((cache) => {
+            console.log('Caching files');
+            return cache.addAll(filesToCache);
+        })
+    );
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
-  );
+    console.log('Fetching:', e.request.url);
+    e.respondWith(
+        caches.match(e.request).then((response) => {
+            return response || fetch(e.request);
+        })
+    );
 });
