144455954bcd5f01e81c1846a67195477ee44c1a
seulgigigi <157334032+seulgigigi@users.noreply.github.com>
2024-03-08 10:22:19 +0800

    Add files via upload

--- todo.js
+++ todo.js
@@ -25,61 +25,7 @@ function addTodo() {
         });
         
         taskInput.value = "";
-        saveTodoToLocalStorage(task); // Save task to localStorage
+        addTask(task); // Add task to IndexedDB
     }
 }
 
-function removeTodoFromLocalStorage(task) {
-    let todoList = getToDoListFromLocalStorage();
-    let index = todoList.indexOf(task);
-    if (index !== -1) {
-        todoList.splice(index, 1);
-        saveToDoListToLocalStorage(todoList); // Save updated todo list to localStorage
-    }
-}
-
-function removeTodoFromDOM(listItem) {
-    listItem.classList.add("remove");
-    setTimeout(() => {
-        listItem.parentNode.removeChild(listItem);
-    }, 500);
-}
-
-function saveTodoToLocalStorage(task) {
-    let todoList = getToDoListFromLocalStorage();
-    todoList.push(task);
-    saveToDoListToLocalStorage(todoList); // Save updated todo list to localStorage
-}
-
-function getToDoListFromLocalStorage() {
-    let todoList = localStorage.getItem("todoList");
-    return todoList ? JSON.parse(todoList) : [];
-}
-
-function saveToDoListToLocalStorage(todoList) {
-    localStorage.setItem("todoList", JSON.stringify(todoList));
-}
-
-function splitTaskIntoLines(task) {
-    let lines = [];
-    let currentLine = "";
-    
-    for (let i = 0; i < task.length; i++) {
-        currentLine += task[i];
-        
-        if (currentLine.length === 35 || task[i] === '\n') {
-            lines.push(currentLine.trim());
-            currentLine = "";
-        }
-    }
-    
-    if (currentLine.trim() !== "") {
-        lines.push(currentLine.trim());
-    }
-    
-    return lines;
-}
-
-function removeAllTodos() {
-    document.getElementById("todoList").innerHTML = "";
-}
