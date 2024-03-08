5a1d59cd18183867c3905a2b4cce3a2751f8f673
seulgigigi <157334032+seulgigigi@users.noreply.github.com>
2024-03-08 10:07:49 +0800

    Add files via upload

--- service.js
+++ service.js
@@ -17,39 +17,65 @@ const filesToCache = [
     '/responsive.css',
     '/todo.js',
     '/pwa.jpeg',
-    '/todostorage.js',
 ];
 
-self.addEventListener('install', (e) => {
-    console.log('Service Worker installed');
-    e.waitUntil(
-        caches.open(cacheName).then((cache) => {
-            console.log('Caching files');
-            return cache.addAll(filesToCache);
+// Install the service worker
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches.open(cacheName)
+        .then((cache) => {
+          // Cache the files
+          return cache.addAll(filesToCache);
         })
     );
 });
 
-self.addEventListener('activate', (e) => {
-    console.log('Service Worker activated');
-    e.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.filter((name) => {
-                    return name !== cacheName;
-                }).map((name) => {
-                    return caches.delete(name);
-                })
-            );
-        })
+// Activate the service worker
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+      caches.keys().then((cacheNames) => {
+        return Promise.all(
+          cacheNames.filter((name) => {
+            return name !== cacheName;
+          }).map((name) => {
+            return caches.delete(name);
+          })
+        );
+      })
     );
 });
 
-self.addEventListener('fetch', (e) => {
-    console.log('Fetching:', e.request.url);
-    e.respondWith(
-        caches.match(e.request).then((response) => {
-            return response || fetch(e.request);
+// Fetch event listener
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.match(event.request)
+        .then((response) => {
+          // Cache hit - return response
+          if (response) {
+            return response;
+          }
+
+          // Clone the request
+          const fetchRequest = event.request.clone();
+
+          // Make the network request
+          return fetch(fetchRequest).then((response) => {
+            // Check if valid response
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // Clone the response
+            const responseToCache = response.clone();
+
+            // Cache the response
+            caches.open(cacheName)
+              .then((cache) => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          });
         })
     );
 });
